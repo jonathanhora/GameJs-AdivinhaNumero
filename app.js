1d23111d87f2b3ac9949c413908c17a9615fd754
Jonathan Hora <jonathan.s.hora@gmail.com>
2024-04-04 15:43:38 -0300

    Alteração e melhoria
    Alteração da mensagem de boas vindas.
    Melhoria: verificando se o número do chute é maior ou menor que o número secreto.

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-//lert('');
-let numeroSecreto = 10;
+alert('Boas vondas ao jogo do número secreto');
 
+let numeroSecreto = 11;
 const numero = parseInt(prompt('Escolha um número entre 1 e 100'))
 
-if(numeroSecreto == numero){  
-    alert('vc acertou')
-}else{  
-    alert('vc errou')
+if (numeroSecreto == numero){  
+    alert(`vc acertou o número secreto ${numeroSecreto}`)
+} else {  
+    if (numeroSecreto > numero){
+        alert(`O número secreto é maior que ${numero}`);
+    } else {
+        alert(`O número secreto é menor que ${numero}`);
+    }
 }
 
