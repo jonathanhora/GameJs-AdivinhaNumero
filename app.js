7943d398979d9b7c14ca0202bd9d532e161c88fe
Jonathan Hora <jonathan.s.hora@gmail.com>
2024-04-04 15:03:16 -0300

    iniciando estrutura

---
+++ app.js
@@ -0,0 +1,11 @@
+//lert('');
+let numeroSecreto = 10;
+
+const numero = parseInt(prompt('Escolha um número entre 1 e 100'))
+
+if(numeroSecreto == numero){  
+    alert('vc acertou')
+}else{  
+    alert('vc errou')
+}
+
