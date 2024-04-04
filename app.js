84150a1fea86cba3e8802e86d137827353015f66
Jonathan Hora <jonathan.s.hora@gmail.com>
2024-04-04 17:08:29 -0300

    Melhoria na estrutura
    Utilização do laço de repetição para verificar se o usuário acertou o número.
    Contabilização das tentativas até o acerto.
    Ajuste na mensagem de acerto.
    Gerando um número aleatório de 1 a 100.

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
 alert('Boas vondas ao jogo do número secreto');
 
-let numeroSecreto = 11;
-const numero = parseInt(prompt('Escolha um número entre 1 e 100'))
+let numeroSecreto = parseInt(Math.random() * 100 +1);
+let numero;
+let tentativa = 1;
 
-if (numeroSecreto == numero){  
-    alert(`vc acertou o número secreto ${numeroSecreto}`)
-} else {  
-    if (numeroSecreto > numero){
-        alert(`O número secreto é maior que ${numero}`);
-    } else {
-        alert(`O número secreto é menor que ${numero}`);
+while (numero != numeroSecreto) {
+
+    numero = parseInt(prompt('Escolha um número entre 1 e 100'));
+
+    if (numeroSecreto == numero){  
+        break;
+    } else {  
+        if (numeroSecreto > numero){
+            alert(`O número secreto é maior que ${numero}`);
+        } else {
+            alert(`O número secreto é menor que ${numero}`);
+        }
+
+        tentativa++;
+
     }
 }
+
+let tentativas = tentativa > 1 ? 'tentativas' : 'tentativa';
+
+alert(`vc acertou o número secreto ${numeroSecreto} em ${tentativa} ${tentativas}`);
+
+
+
 
